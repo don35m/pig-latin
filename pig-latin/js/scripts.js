0799e9b1c0731186f408d101fdb07fd7bb28993d
Don Schemmel & Jenna Navratil <student@epicodus.com>
2015-09-09 11:33:47 -0700

    add next two passing tests and fix if statement

--- pig-latin/js/scripts.js
+++ pig-latin/js/scripts.js
@@ -1,5 +1,9 @@
 var pigLatin = function(wordOne) {
-  if (wordOne[0] = "a" || "e" || "i" || "o" || "u") {
+  if (wordOne[0] === "a" || wordOne[0] === "e" || wordOne[0] === "i" || wordOne[0] === "o" || wordOne[0] === "u") {
     return wordOne.concat("ay");
+  }
+  else {
+    var newWordOne = wordOne.slice(1) + wordOne.slice(0, 1);
+    return newWordOne.concat("ay");
   }
 };
--- pig-latin/spec/specs.js
+++ pig-latin/spec/specs.js
@@ -1,5 +1,13 @@
 describe('pigLatin', function() {
-  it("For words that start with a vowel, add ay to the end.", function() {
-    expect(pigLatin('at')).to.equal('atay');
+  it("For words that start with a, add ay to the end.", function() {
+    expect(pigLatin('at')).to.equal('atay')
+  });
+
+  it("For words that start with e, add ay to the end.", function() {
+    expect(pigLatin('eat')).to.equal('eatay')
+  });
+
+  it("For words that start with a consonant, move first letter to end and add ay.", function() {
+    expect(pigLatin('pig')).to.equal('igpay')
   });
 });
