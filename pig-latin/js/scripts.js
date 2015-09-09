d5554119d8ac80a13114154d8181a9885b48f761
Don Schemmel & Jenna Navratil <student@epicodus.com>
2015-09-09 15:41:03 -0700

    Trying different strategies

--- pig-latin/js/scripts.js
+++ pig-latin/js/scripts.js
@@ -1,9 +1,62 @@
-var pigLatin = function(wordOne) {
-  if (wordOne[0] === "a" || wordOne[0] === "e" || wordOne[0] === "i" || wordOne[0] === "o" || wordOne[0] === "u") {
-    return wordOne.concat("ay");
-  }
-  else {
-    var newWordOne = wordOne.slice(1) + wordOne.slice(0, 1);
-    return newWordOne.concat("ay");
-  }
+// var pigLatin = function(wordOne) {
+//
+// var newWordOne = wordOne.split('');
+// var wordOneLength = wordOne.length;
+//
+//   for (var i = 0; i < wordOneLength; i++) {
+//     if (newWordOne[0] === "a"
+//         || newWordOne[0] === "e"
+//         || newWordOne[0] === "i"
+//         || newWordOne[0] === "o"
+//         || newWordOne[0] === "u") {
+//      return wordOne.concat("ay");
+//      break;
+//    }  else {
+//       newWordOne = wordOne.slice(1) + wordOne.slice(0, 1);
+//       return newWordOne.concat("ay");
+//   }
+//  }
+// };
+
+
+// var pigLatin = function (word) {
+//   var vowels = ["qu","a", "e", "i", "o"];
+//   if (vowels.indexOf(word.charAt(0)) !== -1) {
+//     return word + "ay";
+//   } else if (vowels.indexOf(word.charAt(1)) !== -1) {
+//     return (word.slice(1) + word.slice(0, 1) + "ay");
+//   } else if (vowels.indexOf(word.charAt(2)) !== -1) {
+//     return (word.slice(2) + word.slice(0,2) + "ay");
+//   } else {
+//     return (word.slice(3) + word.slice(0,3) + "ay");
+//   }
+// };
+//
+// var pigLatinQU = function (word) {
+//   var qu = ["qu"];
+//   if (qu.indexOf(qu.charAt(2)) !== -1) {
+//     return (qu.slice(2) + qu.slice(0,2) + "ay");
+//
+//   }
+// };
+
+var pigLatin = function(sentence) {
+  var pigword_array = []
+  sentence.split(" ").forEach(function(word){
+    var word = word.toLowerCase();
+    var me = /^([squ]+|[y]|[^aieouy]+)(\w*)/ ;
+    var pigword = (word.replace(me, "$2$1")).concat("ay");
+    pigword_array.push(pigword) ;
+  });
+  return pigword_array.join(" ")
 };
+
+$(function() {
+  $("form#pig-latin").submit(function(event) {
+    var english = $("input#english").val();
+    var pigword = pigLatin(english);
+    $("#translation").show();
+    $("#piglatin").text(pigword);
+    event.preventDefault();
+  });
+});
--- pig-latin/spec/specs.js
+++ pig-latin/spec/specs.js
@@ -10,4 +10,22 @@ describe('pigLatin', function() {
   it("For words that start with a consonant, move first letter to end and add ay.", function() {
     expect(pigLatin('pig')).to.equal('igpay')
   });
+
+  it("For words that start with 2 consonants, move first 2 letters to end and add ay.", function() {
+    expect(pigLatin('that')).to.equal('atthay')
+  });
+
+  it("For words that start with 3 consonants, move first 3 letters to end and add ay.", function () {
+    expect(pigLatin('strike')).to.equal('ikestray')
+  });
+
+  it("For words that start with qu move first 2 letters to end and add ay.", function () {
+    expect(pigLatin('queen')).to.equal('eenquay')
+  });
+
+  it("For any 2 words add ay to the end.", function () {
+    expect(pigLatin('there home')).to.equal('erethay omehay')
+  });
+
+
 });
